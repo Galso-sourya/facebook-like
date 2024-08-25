4f8f6935dd20b598ae7491f69a438dbf7a3ae60d
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2024-08-25 18:16:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var settingsmenu=document.querySelector(".settings-menu");
+function setingsMenuToggle(){
+settingsmenu.classList.toggle("settings-menu-height");//once we click, the class name will change
+}
